252687058758cfc4dca5f424305217e18c5df890
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-23 16:31:04 +0530

    added a new file of string method

---
+++ Contains.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.*;
+public class Contains{
+    public static void main(String args[]){
+       // Scanner sc = new Scanner(System.in);
+        String s1 = "hello siri how are you";
+        System.out.println(s1.contains("siri"));
+        System.out.println(s1.contains("who"));
+    }
+}
