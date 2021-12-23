fcd40a4c825e6222d1ccddbd83c6cc8141f0136e
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-23 12:49:55 +0530

    added a new file of string method

---
+++ Value2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.*;
+public class Value2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int A = 10;
+        String s1 = String.valueOf(A);
+        System.out.println(s1+30);
+    }
+}
