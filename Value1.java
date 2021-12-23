d1519da4ceab90327ad874accacb0376eb81ae1b
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-23 12:39:59 +0530

    added a new file of String method

---
+++ Value1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class Value1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        boolean b1 = true;
+        int A = 10;
+        char ch = 's';
+        System.out.println(String.valueOf(b1)+" "+String.valueOf(A)+" "+String.valueOf(ch));
+    }
+}
