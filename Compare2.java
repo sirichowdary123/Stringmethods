9646fd1ee15936adcbf6b96a706f6b24f83ec498
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-23 15:32:37 +0530

    added a new file of string method

---
+++ Compare2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.*;
+public class Compare2 {
+    public static void main(String args[]){
+        String s1="siri";
+        String s2="";
+        String s3 ="indu";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s2.compareTo(s3));
+    }
+}
