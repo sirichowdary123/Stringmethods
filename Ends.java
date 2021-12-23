09bba466b3f7bb1baec37a611be73341c3df2b3f
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-23 16:38:48 +0530

    added a new file of string method

---
+++ Ends.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.*;
+public class Ends {
+    public static void main(String args[]){
+        String str = "Learning of String Methods";
+        System.out.println(str.endsWith("d"));
+        System.out.println(str.endsWith("method"));
+        System.out.println(str.endsWith("Methods"));
+        
+    }
+}
