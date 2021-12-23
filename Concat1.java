f678b799e9ea6753c1d6a2137b675881945b4df9
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-23 15:42:38 +0530

    added a new file of string method

---
+++ Concat1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.*;
+public class Concat1 {
+    public static void main(String args[]){
+        String s1 = "Good";
+        String s2 = "Morning";
+        String s3 = "Everyone";
+        String s4 = s1.concat(" ").concat(s2).concat(" ").concat(s3);
+        System.out.println(s4);
+    }
+}
