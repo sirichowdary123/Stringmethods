b2621190e23c7c06b6745bf42dac4a12aacb4c3e
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-24 23:08:17 +0530

    added the new file of string method

---
+++ Format.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Format {
+public static void main(String args[]){
+    String str1 = String.format("%d", 101);           
+        String str2 = String.format("%s", "Amar Singh");   
+        String str3 = String.format("%f", 101.00);         
+        String str4 = String.format("%x", 101);            
+        String str5 = String.format("%c",'c');
+        String str6 = String.format("%010d",10);
+        String str7 = String.format("t",10);
+        String str8 = String.format("a",10.3);
+        String str9 = String.format("%b",0);
+        String str10 = String.format("%b",15);
+        String str11 = String.format("%e",10.4);
+        String str12 = String.format("%g",10.5);
+        String str13 = String.format("%h",'a');
+        String str14 = String.format("%n");
+
+        System.out.println(str1);  
+        System.out.println(str2);  
+        System.out.println(str3);  
+        System.out.println(str4);  
+        System.out.println(str5);  
+        System.out.println(str6);  
+        System.out.println(str7);  
+        System.out.println(str8);  
+        System.out.println(str9);  
+        System.out.println(str10);
+        System.out.println(str11);  
+        System.out.println(str12);  
+        System.out.println(str13);  
+        System.out.println(str14);  
+        
+    }  
+  
+}  
+
